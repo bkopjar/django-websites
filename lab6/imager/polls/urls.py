3ec75dcfa476b04e0a184cb0bb60cd13c3e89d78
bkopjar <bkopjar@etfos.hr>
2019-11-20 18:48:41 +0100

    Add lab6

--- lab6/imager/polls/urls.py
+++ lab6/imager/polls/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    
+    path('', views.IndexView.as_view(), name='index'),
+    path('slika/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    #localhost:8000/slika/3/upvote
+    path('slika/<int:image_id, pk>/upvote', views.upvote, name="upvote"),
+    path('slika/<int:image id, pk>/upvote', views.downvote, name="downvote"),
 ]
--- lab6/imager/polls/views.py
+++ lab6/imager/polls/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from polls.models import Image
+from django.urls import reverse
 # Create your views here.
-def index(request):
-    return HttpResponse("Index")
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'image_list'
+
+    def get_queryset(self):
+        return Image.objects.all
+
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Image
+
+def upvote(request, image_id):
+    image = get_object_or_404(Image, pk=image_id)
+    image.votes += 1
+    image.save()
+    return HttpResponseRedirect(reverse('polls:index'))
+
+
+def downvote(request, image_id):
+    image = get_object_or_404(Image, pk=image_id)
+    image.votes -= 1
+    image.save()
+    return HttpResponseRedirect(reverse('polls:index'))
+
 
