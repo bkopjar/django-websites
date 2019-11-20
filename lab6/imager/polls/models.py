9dc69dae209c088a1e7d3ea6e5d510aceb164e93
bkopjar <bkopjar@etfos.hr>
2019-11-20 17:20:41 +0100

    Add lab6

---
+++ lab6/imager/polls/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField('date_published')
+    votes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 50)
+    comment = models.CharField(max_length = 1000)
+    pub_date = models.DateTimeField('date_published')
+
+
